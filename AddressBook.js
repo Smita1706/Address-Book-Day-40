6e2f601052eb77f24cf39b5db94f8edd311507c4
Smita1706 <smitachinchkar1998@gmail.com>
2022-04-02 16:13:32 +0530

    Refactored : The use case 2 is refactored

--- AddressBook.js
+++ AddressBook.js
@@ -1,18 +1,12 @@
+let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+const addressRegex = RegExp('^[a-zA-Z0-9#,]{4,}$');
+const cityStateRegex = RegExp('^[a-zA-Z]{4,}$');
+const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+const phoneNumberRegex = RegExp('^\\d{2}(\\s{1}\\d{10})$');
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
 
-console.log("Welcome to The Address Book JS Program ") 
-class Contact
-{
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phoneNo;
-    email;
-
-    constructor(...params)
-    {
+class Contact {
+    constructor(...params) {
         this.firstName = params[0];
         this.lastName = params[1];
         this.address = params[2];
@@ -23,74 +17,85 @@ class Contact
         this.email = params[7];
     }
 
-    get firstName()
-    { 
-        return this._firstName
-    }
-    set firstName(firstName)
-    {
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(firstName))
-            this._firstName = firstName
-        else throw "Incorrect First Name"
-    }
-    get lastName(){ return this._lastName}
-    set lastName(lastName){
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-        if(nameRegex.test(lastName))
-            this._lastName = lastName
-        else throw "Incorrect Last Name"
-    }
-    get address(){ return this._address}
-    set address(address){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(address))
-            this._address = address
-        else throw "Incorrect Address"
-    }
-    get city(){ return this._city}
-    set city(city){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(city))
-            this._city = city
-        else throw "Incorrect City"
-    }
-    get state(){ return this._state}
-    set state(state){
-        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
-        if(regex.test(state))
-            this._state = state
-        else throw "Incorrect State"
-    }
-    get zip(){ return this._zip}
-    set zip(zip){
-        let letterRegex = RegExp('^[^a-zA-z]+$')
-        let splRegex = RegExp('^[!@#$^&%*()+=-[]\/{}|:<>?,.]+$')
-        let pinRegex = RegExp('^[0-9]{3}[\\s]{0,1}[0-9]{3}$')
-        if(letterRegex.test(zip) && !splRegex.test(zip) && pinRegex.test(zip)){
-            zip = parseInt(zip.replace(" ",""))
-            this._zip = zip
-        }
-        else throw "Incorrect Zip"
-    }
-    get phoneNo(){ return this._phoneNo}
-    set phoneNo(phoneNo){
-        let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
-        if(regex.test(phoneNo))
-            this._phoneNo = phoneNo
-        else throw "Incorrect Phone Number"
-    }
-    get email(){ return this._email}
-    set email(email){
-        let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
-        if(regex.test(email))
-            this._email = email
-        else throw "Incorrect Email"
+    get firstName() {
+        return this._firstName;
     }
 
+    set firstName(firstName) {
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else
+            throw "FIRST NAME is Incorrect ";
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(lastName) {
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else
+            throw "LAST NAME is Incorrect ";
+    }
+
+    get address() {
+        return this._address;
+    }
+    set address(address) {
+        if (addressRegex.test(address))
+            this._address = address;
+        else
+            throw "ADDRESS is Incorrect";
+    }
+    get city() {
+        return this._city;
+    }
+    set city(city) {
+        if (cityStateRegex.test(city))
+            this._city = city;
+        else
+            throw "CITY is Incorrect";
+    }
+    get state() {
+        return this._state;
+    }
+
+    set state(state) {
+        if (cityStateRegex.test(state))
+            this._state = state;
+        else
+            throw "STATE is Incorrect";
+    }
+    get zip() {
+        return this._zip;
+    }
+    set zip(zip) {
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw "ZIP is Incorrect";
+    }
+    get phoneNumber() {
+        return this._phoneNo;
+    }
+    set phoneNumber(phoneNumber) {
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNo = phoneNumber;
+        else
+            throw "PHONE NUMBER is Incorrect";
+    }
+    get email() {
+        return this._email;
+    }
+    set email(email) {
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "EMAIL ADDRESS is Incorrect";
+    }
 }
+
 try{
-let contact1 = new Contact("Riya", "Jadhav", "Hadapsar", "Pune","Maharashtra", 412207, 9988776655, "riya@gmail.com");
+let contact1 = new Contact("riya", "Jadhav", "Hadapsar", "Pune","Maharashtra", 412207, '91 9988776655', "riya@gmail.com");
 console.log(contact1);
 }catch(e){
     console.log(e);
